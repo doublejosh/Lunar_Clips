d747f6db1c098b9f5c99e3f013c256757aa8966f
Josh Lind <joshuaklind@gmail.com>
2012-11-24 12:29:34 -0800

    begin with adding red surges to the standard pulse

--- Lunar_Clips.ino
+++ Lunar_Clips.ino
@@ -4,9 +4,10 @@
 // Visual Settings...
 int color1[] = {40, 50, 120};
 int color2[] = {255, 255, 255};
-float bpm = 70;
-float steps = 20;
-int randomness = 10;
+float bpm = 70; // beats per minute.
+float cph = 120; // cycles per hour.
+float steps = 20; // steps between colors (bg).
+int randomness = 0; // color steps to jump around while fading (bg).
 
 // System Settings...
 int dataPin = 2;
@@ -14,7 +15,7 @@ int clockPin = 3;
 WS2801 strip = WS2801(20, dataPin, clockPin);
 boolean debug_mode = true;
 unsigned int cycles = 0;
-float waitFloat = (((60 / bpm) / (steps *2 )) * 1000);
+float waitFloat = (((60 / bpm) / (steps * 2)) * 1000);
 int wait = (int) waitFloat;
 
 // Required Functions...
@@ -31,6 +32,7 @@ void setup() {
 }
 
 void loop() {
+  
   int i;
   for (i=0; i < steps*2; i++) {
     if (i >= steps) {
@@ -39,6 +41,8 @@ void loop() {
     else {
       colorPicker(i);
     }
+    
+    cyclist();
     
     strip.show();
     delay(wait);
@@ -50,30 +54,44 @@ void loop() {
 
 // Pulse blueish for a while, then sprinkle in red, and finally bloom, then return.
 // Purlse intensity as well. Add in color fluctuation if possible.
+
+// Larger pattern.
+void cyclist() {
+  if (cycles % 120 < 8) {
+    colorWipe(Color(255, 0, 0));
+  }
+  else {
+      
+  }
+  cycles++;
+}
+
+// Pulse blue picker.
 void colorPicker(int i) {
 
   int red = ((abs(color1[0] - color2[0]) / steps) * i) + color1[0];
   int green = ((abs(color1[1] - color2[1]) / steps) * i) + color1[1];
   int blue = ((abs(color1[2] - color2[2]) / steps) * i) + color1[2];
   
-  switch (random(0,3)) {
-    case 0: 
-      red = red - random(0,randomness);
-      break;
-      
-    case 1:
-      green = green - random(0,randomness);
-      break;
-      
-    case 2:
-      blue = blue - random(0,randomness);
-      break;
+  if (randomness) {
+    switch (random(0,3)) {
+      case 0: 
+        red = red - random(0,randomness);
+        break;
+        
+      case 1:
+        green = green - random(0,randomness);
+        break;
+        
+      case 2:
+        blue = blue - random(0,randomness);
+        break;
+    }
   }
   
   colorWipe(Color(red, green, blue));
   
   if(debug_mode) {
-    cycles++;
     String msg = "<< << << #";
     msg += cycles;
     msg += " w:";
@@ -83,6 +101,7 @@ void colorPicker(int i) {
 
 }
 
+// Set the background color.
 void colorWipe(uint32_t c) {
   int i;
   for (i=0; i < strip.numPixels(); i++) {   
@@ -90,6 +109,7 @@ void colorWipe(uint32_t c) {
   }
 }
 
+// Define a color.
 uint32_t Color(byte r, byte g, byte b)
 {
   uint32_t c;
@@ -101,6 +121,7 @@ uint32_t Color(byte r, byte g, byte b)
   return c;
 }
 
+// Debugger.
 void report (int r, int g, int b, String msg) {
   if(debug_mode) {
     Serial.print("r: ");
